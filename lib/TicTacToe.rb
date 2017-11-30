6e85815e2986e00d363e8364bf833d8f574bd768
Nadiia Berdnyk <nadya.berdnik@mail.ru>
2017-11-30 15:23:48 +0200

    Add files

---
+++ lib/TicTacToe.rb
@@ -0,0 +1,11 @@
+require "TicTacToe/version"
+
+module TicTacToe
+  # Your code goes here...
+end
+
+require_relative "./TicTacToe/cell.rb"
+require_relative "./TicTacToe/player.rb"
+require_relative "./TicTacToe/board.rb"
+require_relative "./TicTacToe/core_extensions.rb"
+require_relative "./TicTacToe/game.rb"
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require "bundler/setup"
+require "TicTacToe"
+require_relative "../lib/TicTacToe.rb"
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+  config.expose_dsl_globally = true
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
